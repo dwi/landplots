b2fc8ac9cb7fe8f390cf22edf81b04ec10546068
dwi <omg@upal.se>
2022-07-10 17:41:24 +0000

    add owned and staked

--- index.js
+++ index.js
@@ -13,24 +13,35 @@ const landStakingPoolABI = require("./LandStakingPool.json")
 const landContract = new ethers.Contract('0x8c811e3c958e190f5ec15fb376533a3398620500', landABI, provider);
 const landStakingPoolContract = new ethers.Contract('0xb2a5110f163ec592f8f0d4207253d8cbc327d9fb', landStakingPoolABI, provider);
 
+
+const findLand = (row, col) => {
+    return landTypes.find(x => x.row === row && x.col === col)
+}
 app.get('/land/:address', async (req, res) => {
-    const address = toChecksumAddress(req.params.address)
-    if (address && ethers.utils.isAddress(address)) {
-        const landList = await landStakingPoolContract.getStakedLands(address);
-        const results = { address: address, lands: [] }
-        const tempArr = []
-
-        for (var i = 0; i < landList.length; i++) {
-            tempArr.push(landContract.decodeTokenId(landList[i]))
+    try {
+        const address = toChecksumAddress(req.params.address.replace('ronin:', '0x'))
+        if (address && ethers.utils.isAddress(address)) {
+            const landList = await landStakingPoolContract.getStakedLands(address);
+            const landListWallet = await landContract.landOfOwner(address);
+            const results = { address: address, own: [], staked: [] }
+            const tempArr = []
+            for (var i = 0; i < landList.length; i++) {
+                tempArr.push(landContract.decodeTokenId(landList[i]))
+            }
+            const lands = await Promise.all(tempArr)
+            lands.forEach(item => {
+                results.staked.push(findLand(Number(item._row), Number(item._col)))
+            })
+            for (var i = 0; i < landListWallet[0].length; i++) {
+                results.own.push(findLand(Number(landListWallet[0][i]), Number(landListWallet[1][i])))
+            }
+            res.status(200).send(results)
+        } else {
+            res.status(404).send('Missing address')
         }
-        const lands = await Promise.all(tempArr)
-        lands.forEach(item => {
-            results.lands.push(landTypes.find(x => x.col === Number(item._col) && x.row === Number(item._row)))
-        })
-
-        res.status(200).send(results)
-    } else {
-        res.status(404).send('Missing address')
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err.message)
     }
 })
 
