0f552ad38acba06aae27ee1c9ad7add00d142fb1
dwi <omg@upal.se>
2022-08-08 16:44:54 +0000

    web3 instead of ethers

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const cors = require('cors')
-const ethers = require('ethers');
-const { toChecksumAddress } = require('ethereum-checksum-address')
+const Web3 = require('web3');
 const app = express()
 const port = 3200
 
 const url = 'https://api.roninchain.com/rpc';
-const provider = new ethers.providers.JsonRpcProvider(url)
+const web3 = new Web3(new Web3.providers.HttpProvider(url))
 
 const landTypes = ["Savannah", "Forest", "Artic", "Mystic", "Genesis"]
 const landGridData = require("./Land_Grid_Data_Multiplier.json")
 const landABI = require("./LandContract.json")
 const landStakingPoolABI = require("./LandStakingPool.json")
-const landContract = new ethers.Contract('0x8c811e3c958e190f5ec15fb376533a3398620500', landABI, provider);
-const landStakingPoolContract = new ethers.Contract('0xb2a5110f163ec592f8f0d4207253d8cbc327d9fb', landStakingPoolABI, provider);
+const landContract = new web3.eth.Contract(landABI, '0x8c811e3c958e190f5ec15fb376533a3398620500');
+const landStakingPoolContract = new web3.eth.Contract(landStakingPoolABI, '0xb2a5110f163ec592f8f0d4207253d8cbc327d9fb');
 
 var corsOptionsDelegate = function (req, callback) {
     callback(null, { origin: true })
@@ -25,14 +24,14 @@ const findLand = (row, col) => {
 }
 app.get('/land/:address', async (req, res) => {
     try {
-        const address = toChecksumAddress(req.params.address.replace('ronin:', '0x'))
-        if (address && ethers.utils.isAddress(address)) {
-            const landList = await landStakingPoolContract.getStakedLands(address);
-            const landListWallet = await landContract.landOfOwner(address);
+        const address = Web3.utils.toChecksumAddress(req.params.address.replace('ronin:', '0x'))
+        if (address && Web3.utils.isAddress(address)) {
+            const landList = await landStakingPoolContract.methods.getStakedLands(address).call();
+            const landListWallet = await landContract.methods.landOfOwner(address).call();
             const results = { address: address, stakedStats: [], owned: [], staked: [] }
             const tempArr = []
             for (var i = 0; i < landList.length; i++) {
-                tempArr.push(landContract.decodeTokenId(landList[i]))
+                tempArr.push(landContract.methods.decodeTokenId(landList[i]).call())
             }
             const lands = await Promise.all(tempArr)
             lands.forEach(item => {
